05739bf54071f94b234addc405e8babccd91f92a
unun9 <76216851+unun9@users.noreply.github.com>
2021-02-03 19:23:31 +0300

    hom work 3

--- qw.py
+++ qw.py
@@ -1,15 +1,58 @@
-# # 11.112
-# zx=(23,45,34,34,1,3,5,8,0,567,3456,456)
-# print("первый вариант \nдано массив "+str(zx))
-# zx=sorted(zx)
-# print('min - '+str(zx[0]),'max - '+str(zx[-1]))
-# print(" индекс  max[-1] or max[11]    индекс min[0]")
-# print("второй вариант ")
-# print('дано массив - ',str(zx))
-# print('min - '+str(min(zx)),'max - '+str(max(zx)),"розница max-min = ",max(zx)-min(zx),)
-'''a,s=map(int,input('hgfdsdfgh').split())
-print(a)
-print(s)'''
+print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
 
-# 11.111
-mass = (0, 45, 3, 23, 5, 4, 6, 8, 10, 12, 5, 7, 4, 2, 4, 6, 8, 10, 12, 14)
+board = list(range(1,10))
+
+def draw_board(board):
+   print("-" * 13)
+   for i in range(3):
+      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+      print("-" * 13)
+
+def take_input(player_token):
+   valid = False
+   while not valid:
+      player_answer = input("Куда поставим " + player_token+"? ")
+      try:
+         player_answer = int(player_answer)
+      except:
+         print("Некорректный ввод. Вы уверены, что ввели число?")
+         continue
+      if player_answer >= 1 and player_answer <= 9:
+         if(str(board[player_answer-1]) not in "XO"):
+            board[player_answer-1] = player_token
+            valid = True
+         else:
+            print("Эта клетка уже занята!")
+      else:
+        print("Некорректный ввод. Введите число от 1 до 9.")
+
+def check_win(board):
+   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
+   for each in win_coord:
+       if board[each[0]] == board[each[1]] == board[each[2]]:
+          return board[each[0]]
+   return False
+
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+           take_input("X")
+        else:
+           take_input("O")
+        counter += 1
+        if counter > 4:
+           tmp = check_win(board)
+           if tmp:
+              print("Ураааааа !!!!!   "+tmp+"  выиграл!")
+              win = True
+              break
+        if counter == 9:
+            print("Ничья!")
+            break
+    draw_board(board)
+main(board)
+
+input("Нажмите Enter для выхода!")
